53f80538df5defe155472c2987ed1e297b4ad97f
saha23s <saha23s@mtholyoke.edu>
2022-01-31 15:58:12 -0500

    prework

---
+++ Prework-Sulagna/Prework-Sulagna/Prework-SulagnaUITests/Prework_SulagnaUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Prework_SulagnaUITestsLaunchTests.swift
+//  Prework-SulagnaUITests
+//
+//  Created by 조은이꺼 on 1/31/22.
+//
+
+import XCTest
+
+class Prework_SulagnaUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
